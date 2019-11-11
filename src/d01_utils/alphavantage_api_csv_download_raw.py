383855cf7b85281157b1e7ef4ebad43a3d2d84d8
chadlis <salah.chadli@gmail.com>
2019-11-11 15:17:16 +0100

    "finalise extraction function file"

--- src/d01_utils/alphavantage_api_csv_download_raw.py
+++ src/d01_utils/alphavantage_api_csv_download_raw.py
@@ -1,7 +1,7 @@
+import pandas as pd
 def alphavantage_api_csv_download_raw(function, symbol, alpha_vantage_key):
     function = function
     symbol = symbol
     datatype = 'csv'
-    url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}\
-          &datatype={datatype}&apikey={alpha_vantage_key}"
+    url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&datatype={datatype}&apikey={alpha_vantage_key}"
     return pd.read_csv(url)
